5723fed16eafe2b93302157c5ed015e43788f833
MortalityWatch <mortalitywatch@proton.me>
2024-03-09 11:48:09 -0800

    fix cors

--- src/serve.r
+++ src/serve.r
@@ -5,6 +5,7 @@ library(fiery)
 
 port <- ifelse(Sys.getenv("PORT") != "", Sys.getenv("PORT"), "3000")
 app <- Fire$new(host = "0.0.0.0", port = as.integer(port))
+app$header("Access-Control-Allow-Origin", "*")
 
 app$on("request", function(server, request, ...) {
   y <- as.double(strsplit(request$query$y, ",")[[1]])
