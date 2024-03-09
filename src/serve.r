c55129fd4e28c19f66e75da385df76da0997284e
MortalityWatch <mortalitywatch@proton.me>
2024-03-09 13:24:35 -0800

    fixes

--- src/serve.r
+++ src/serve.r
@@ -15,8 +15,10 @@ app$on("request", function(server, request, ...) {
   s <- as.integer(request$query$s) # Year = 1, Quarter = 2, ...
 
   # Sample
-  # y <- c(277.9, 256.9, 251, 272.2, 284.9, 258.7, 250.1, 264.4, 273.2, 253.9, 250, 272.1, 291.6, 253, 244.8, 262.8, 273.8, 253.7, 246.3, 269.8, 287.3, 254.8, 245.8, 259, 274, 251.6, 245.4, 263.9, 280.2, 252.5, 244.5, 264.5, 280.2, 247.9, 241, 258.1, 268.4, 250.7, 243.5, 263.1)
-  # h <- 3
+  # y <- c(709.9, 684.9, 686.9)
+  # h <- 5
+  # s <- 1
+  # t <- FALSE
 
   df_bl <- tibble(x = seq.int(1, length(y)), y = y)
   if (s == 2) {
@@ -28,17 +30,14 @@ app$on("request", function(server, request, ...) {
   }
 
   df_bl <- df_bl |> as_tsibble(index = x)
-  df_bl$t <- rep(1:4, length.out = length(y))
-  df_bl |> model(TSLM(y ~ season()))
-
   if (t) {
-    if (s) {
+    if (s > 1) {
       m <- df_bl |> model(TSLM(y ~ trend() + season()))
     } else {
       m <- df_bl |> model(TSLM(y ~ trend()))
     }
   } else {
-    if (s) {
+    if (s > 1) {
       m <- df_bl |> model(TSLM(y ~ season()))
     } else {
       m <- df_bl |> model(TSLM(y))
