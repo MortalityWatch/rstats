054a28db7c0b92dd8697d04e81ca7af36eb64193
MortalityWatch <mortalitywatch@proton.me>
2024-03-09 13:38:16 -0800

    add naive

--- src/serve.r
+++ src/serve.r
@@ -13,12 +13,14 @@ app$on("request", function(server, request, ...) {
   h <- as.integer(request$query$h)
   t <- as.logical(request$query$t)
   s <- as.integer(request$query$s) # Year = 1, Quarter = 2, ...
+  n <- as.logical(request$query$n) # Naive
 
   # Sample
   # y <- c(709.9, 684.9, 686.9)
   # h <- 5
   # s <- 1
   # t <- FALSE
+  # n <- TRUE
 
   df_bl <- tibble(x = seq.int(1, length(y)), y = y)
   if (s == 2) {
@@ -29,18 +31,19 @@ app$on("request", function(server, request, ...) {
     df_bl$x <- make_yearweek(2000, 1) + 0:(length(y) - 1)
   }
 
+  fun <- ifelse(n, NAIVE, TSLM)
   df_bl <- df_bl |> as_tsibble(index = x)
   if (t) {
     if (s > 1) {
-      m <- df_bl |> model(TSLM(y ~ trend() + season()))
+      m <- df_bl |> model(fun(y ~ trend() + season()))
     } else {
-      m <- df_bl |> model(TSLM(y ~ trend()))
+      m <- df_bl |> model(fun(y ~ trend()))
     }
   } else {
     if (s > 1) {
-      m <- df_bl |> model(TSLM(y ~ season()))
+      m <- df_bl |> model(fun(y ~ season()))
     } else {
-      m <- df_bl |> model(TSLM(y))
+      m <- df_bl |> model(fun(y))
     }
   }
 
@@ -51,9 +54,9 @@ app$on("request", function(server, request, ...) {
     unpack_hilo(cols = `95%`) |>
     as_tibble() |>
     select(.mean, "95%_lower", "95%_upper") |>
-    setNames(c("y", "lower", "upper")) |>
+    setNames(c("y", "lower", "upper"))
+  result <- bind_rows(tibble(y = bl$.mean), result) |>
     mutate_if(is.numeric, round, 1)
-  result <- bind_rows(tibble(y = bl$.mean), result)
 
   response <- request$respond()
   response$body <- jsonlite::toJSON(
