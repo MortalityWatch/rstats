af1e10fc8c3ebdcaa8c5df3e40f7bcba3ca239cb
MortalityWatch <mortalitywatch@proton.me>
2025-04-13 14:32:49 -0400

    allow cors

--- deploy_prepare.sh
+++ deploy_prepare.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
+ssh co "dokku plugin:install https://github.com/USMortality/dokku-nginx-cors.git nginx-cors"
+ssh co "dokku nginx-cors:enable rstats-mortality-watch"
 ssh co "dokku plugin:install https://github.com/USMortality/dokku-nginx-cache nginx-cache"
 ssh co "dokku nginx-cache:enable ${APP_NAME}"
